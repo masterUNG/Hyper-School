0363d4a8ca9db1c285dbf4ec9d2013f4e123bbb0
masterUNG <phrombutr@gmail.com>
2016-10-25 15:16:04 +0700

    After Lunch2

--- app/src/main/java/appewtc/masterung/hyperschool/MainActivity.java
+++ app/src/main/java/appewtc/masterung/hyperschool/MainActivity.java
@@ -1,13 +1,28 @@
 package appewtc.masterung.hyperschool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+
+
+    }   // Main Method
+
+
+}   // Main Class นี่คือคลาสหลัก
