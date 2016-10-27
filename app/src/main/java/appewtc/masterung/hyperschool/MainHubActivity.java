0e8a5ffeaf6d9dacf3ae271b1d9a1c1d79e45174
masterUNG <phrombutr@gmail.com>
2016-10-27 16:04:11 +0700

    Finish 27oct

--- app/src/main/java/appewtc/masterung/hyperschool/MainHubActivity.java
+++ app/src/main/java/appewtc/masterung/hyperschool/MainHubActivity.java
@@ -1,11 +1,13 @@
 package appewtc.masterung.hyperschool;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainHubActivity extends AppCompatActivity {
+public class MainHubActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private TextView textView;
@@ -35,7 +37,65 @@ public class MainHubActivity extends AppCompatActivity {
         String[] positionStrings = new String[]{"นักเรียน", "อาจารย์"};
         textView.setText(loginStrings[4] + " " + positionStrings[Integer.parseInt(loginStrings[1])]);
 
+        //Image Controller
+        sub1ImageView.setOnClickListener(MainHubActivity.this);
+        sub2ImageView.setOnClickListener(MainHubActivity.this);
+        sub3ImageView.setOnClickListener(MainHubActivity.this);
+        sub4ImageView.setOnClickListener(MainHubActivity.this);
+        sub5ImageView.setOnClickListener(MainHubActivity.this);
+        sub6ImageView.setOnClickListener(MainHubActivity.this);
+
 
     }   // Main Method
 
+    @Override
+    public void onClick(View view) {
+
+        int index = 0;
+
+        //For Teacher
+        if (Integer.parseInt(loginStrings[1]) == 1) {
+
+            switch (view.getId()) {
+
+                case R.id.imageView2:
+                    index = 0;
+                    break;
+                case R.id.imageView3:
+                    index = 1;
+                    break;
+                case R.id.imageView4:
+                    index = 2;
+                    break;
+                case R.id.imageView5:
+                    index = 3;
+                    break;
+                case R.id.imageView6:
+                    index = 4;
+                    break;
+                case R.id.imageView7:
+                    index = 5;
+                    break;
+
+            }   // switch
+
+            Intent intent = new Intent(MainHubActivity.this, TeacherService.class);
+            intent.putExtra("Login", loginStrings);
+            intent.putExtra("Index", index);
+            startActivity(intent);
+
+
+        }   // if
+
+
+    }   // onClick
+
 }   // Main Class นี่คือคลาสหลัก นะจร้า
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/appewtc/masterung/hyperschool/MyConstant.java
@@ -0,0 +1,21 @@
+package appewtc.masterung.hyperschool;
+
+/**
+ * Created by masterUNG on 10/27/2016 AD.
+ */
+
+public class MyConstant {
+
+    //Explicit
+    private String[] subjectStrings = new String[]{
+            "เคมี",
+            "พละ",
+            "สุขศึกษา",
+            "ภาษาไทย",
+            "เลข",
+            "การงาน"};
+
+    public String[] getSubjectStrings() {
+        return subjectStrings;
+    }
+}   // Main Class
